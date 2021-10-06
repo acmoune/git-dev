99987759e5add3ec7635ff30c083845a85df2fa1
Alain Moune <ac.moune@gmail.com>
2021-10-06 14:40:22 +0100

    Fixed visibility

--- src/book/Book.ts
+++ src/book/Book.ts
@@ -1,25 +1,25 @@
 export class Book {
   private _title: string;
-  public get title(): string {
+  get title(): string {
     return this._title;
   }
-  public set title(title: string) {
+  set title(title: string) {
     this._title = title;
   }
 
   private _authors: string[];
-  public get authors(): string[] {
+  get authors(): string[] {
     return this._authors;
   }
-  public set authors(authors: string[]) {
+  set authors(authors: string[]) {
     this._authors = authors;
   }
 
   private _publicationDate: Date;
-  public get publicationDate(): Date {
+  get publicationDate(): Date {
     return this._publicationDate;
   }
-  public set publicationDate(publicationDate: Date) {
+  set publicationDate(publicationDate: Date) {
     this._publicationDate = publicationDate;
   }
 }
