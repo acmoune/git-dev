96d373cf2f1dff8718b7aec3399ecb7b6c371b8a
Alain Moune <ac.moune@gmail.com>
2021-10-06 14:15:29 +0100

    Add substract method

--- src/calculator/Calculator.ts
+++ src/calculator/Calculator.ts
@@ -2,4 +2,8 @@ export class Calculator {
   public add(left: number, right: number): number {
     return left + right;
   }
+
+  public substract(left: number, right: number): number {
+    return left - right;
+  }
 }
